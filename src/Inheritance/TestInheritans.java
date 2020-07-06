2267da0649cc02c01d9f4f40a784ec8136b46cf8
Yelena1212 <Baranchuk1212@gmail.com>
2020-07-05 22:22:48 -0700

    initial commit

---
+++ src/Inheritance/TestInheritans.java
@@ -0,0 +1,21 @@
+package Inheritance;
+
+public class TestInheritans {
+
+	public static void main(String[] args) {
+		
+		var dog = new Dog("Small dog", "Tuk", "Chihuahua");
+		dog.bark();
+// method overriding
+		dog.eat();
+		System.out.println("Dog type is" + dog.getType());
+		
+		
+		
+		Animal animal = new Dog("Very big dog", "Rex", "Labrador");
+		animal.eat();
+		((Dog)animal).bark();
+
+	}
+
+}
